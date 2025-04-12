c7fcecc4bc17f0196b3d28844c6951f7ce376a17
Nobin <nobinsijo360t@gmail.com>
2025-04-12 17:49:04 +0530

    Image Updated

--- components/Blocks/Resume/Resume.jsx
+++ components/Blocks/Resume/Resume.jsx
@@ -50,7 +50,7 @@ export default function Resume() {
                         <div className={`${styles.card} ${styles.cardV1}`}>
                             <div className={styles.cardInner}>
                                 <div className={styles.cardTitle}>Nobin Sijo</div>
-                                <div className={styles.cardDesc}>Senior Frontend Developer</div>
+                                <div className={styles.cardDesc}>UI/UX Designer|Frontend Developer</div>
                                 <hr/>
                                 <p>I'm Nobin, a frontend developer with over 2 years of professional experience. Successfully designing websites and apps with Figma, then building user-friendly websites with ReactJS & NextJS. I pay attention to small details, enjoy technical problems, and work well in teams. I like learning new technologies and always aim to improve my skills.
                                 </p>
@@ -69,7 +69,7 @@ export default function Resume() {
                                 </p>
                             </div>
                             <figure className={styles.figure}>
-                                <Image src="/yasin-genc-photo.jpeg" alt="Nobin" width={150} height={150}/>
+                                <Image src="/nobin-sijo.webp" alt="Nobin" width={150} height={150}/>
                             </figure>
                         </div>
                         <div className={`${styles.card} ${styles.cardV2}`}>
