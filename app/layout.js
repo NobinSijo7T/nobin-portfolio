ee7a9d55333b44ffcdd0e374da9c4134800382c1
Nobin <nobinsijo360t@gmail.com>
2025-04-13 20:22:04 +0530

    Favicon Added

--- app/layout.js
+++ app/layout.js
@@ -24,12 +24,8 @@ export const metadata = {
     title: commonConfig.metadata.title,
     description: commonConfig.metadata.description,
     icons: {
-        icon: [
-            { url: '/favicon.ico', sizes: 'any' },
-            { url: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-            { url: '/icon-512.png', sizes: '512x512', type: 'image/png' },
-        ],
-        apple: { url: '/apple-icon.png', sizes: '180x180', type: 'image/png' },
+        icon: { url: '/icon-192.png', type: 'image/png' },
+        apple: { url: '/apple-icon.png', sizes: '180x180', type: 'image/png' }
     }
 }
 
